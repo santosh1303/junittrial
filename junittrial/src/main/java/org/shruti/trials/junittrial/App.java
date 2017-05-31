ca4b458c88e8c466d46ed5b2c6a2d9d03c26d04d
Shruti Gupta <shruti.g4@gmail.com>
2017-05-31 14:50:09 -0400

    initial commit

---
+++ junittrial/src/main/java/org/shruti/trials/junittrial/App.java
@@ -0,0 +1,18 @@
+package org.shruti.trials.junittrial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int add2Numbers(int intVariable1, int intVariable2)
+	{
+		return intVariable1 + intVariable2;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ junittrial/src/test/java/org/shruti/trials/junittrial/AppTest.java
@@ -0,0 +1,41 @@
+package org.shruti.trials.junittrial;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	App objApp = new App();
+    	
+    	assertEquals(5, objApp.add2Numbers(2, 3));
+        
+    }
+}
